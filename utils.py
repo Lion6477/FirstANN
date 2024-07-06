689ed32347d2148a941377cf3aef63ba25702420
Serhii <sekret7d@gmail.com>
2024-07-06 17:20:14 +0200

    Update utils.py
    fix reshape array of pictures

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def load_dataset():
 
         #reshape from (60000, 28, 28) to (60000, 784)
         # x_train = x_train.reshape(-1, 784)
-        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1]))
+        x_train = x_train.reshape(-1, 784)
 
         #labels
         y_train = f['y_train']
